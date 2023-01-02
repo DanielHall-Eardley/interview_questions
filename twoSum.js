ec72de72e28bea578b906ee869ec4ba70ba1a948
DanielHall-Eardley <350chevy8@gmail.com>
2023-01-02 09:46:37 -0500

    sync

--- twoSum.js
+++ twoSum.js
@@ -1,10 +1,7 @@
 //given an array of integers find sum of two elements that match a given number.
 
-// target = 6
-//
-// [1, 5, 7, 10, 12]
-
-function targetSum(arr, target) {
+// Sorted array
+function twoSumSorted(arr, target) {
   let start = 0;
   let end = arr.length - 1;
 
@@ -27,11 +24,48 @@ function targetSum(arr, target) {
   return false;
 }
 
-// should return [2, 4]
+// should return [2, 5]
 const array = [1, 2, 5, 5, 6, 12, 22];
 const target = 17;
 
-const result = targetSum(array, target);
+const result = twoSumSorted(array, target);
 console.log(result);
 
-// O (n/2)
+// O(n/2)
+
+// Unsorted array brute force
+function twoSumUnsorted(array, target) {
+  if (array.length < 2) {
+    return false;
+  }
+
+  for (let i = 0; i < array.length; i++) {
+    const leftNum = array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      const rightNum = array[j];
+      const sum = leftNum + rightNum;
+      if (sum === target) {
+        return [i, j];
+      }
+    }
+  }
+
+  return false;
+}
+
+// should return [3, 5]
+const arrayUnsorted = [6, 1, 2, 12, 22, 5];
+
+const resultUnsorted = twoSumUnsorted(arrayUnsorted, target);
+console.log(resultUnsorted);
+
+// O(n * m - 1)
+
+// Unsorted array optimized
+function twoSumOptimized(array, target) {
+  if (array.length < 2) {
+    return false;
+  }
+
+  return false;
+}
